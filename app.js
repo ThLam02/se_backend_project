42c197eed47d2f5d71c44a12babb67fce7e1bdc8
Bui Thanh Lam <buitthanhlam.050702@gmail.com>
2022-11-26 22:29:41 +0700

    init contain signup in validator

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
+const expressValidator = require("express-validator");
 require("dotenv").config();
 // import routes
 const userRoutes = require("./routes/user");
@@ -17,11 +18,12 @@ mongoose.connect(process.env.DATABASE, ()=> {console.log("DB Connected")});
 app.use(morgan("dev"));
 app.use(bodyParser.json());
 app.use(cookieParser());
+app.use(expressValidator());
 
 // routes middleware
 app.use("/api", userRoutes);
 
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
